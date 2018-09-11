51739ac116f511b942c14a7b166de23e01881e2d
KenDinh <d7n1b@ugrad.cs.ubc.ca>
2018-09-10 23:57:54 -0700

    Finished P1

---
+++ src/ui/Main.java
@@ -0,0 +1,15 @@
+package ui;
+
+public class Main {
+    public static void main(String[] args) {
+        printString();
+        printResult();
+    }
+    public static void printString() {
+        System.out.println("Hello World");
+    }
+
+    public static void printResult() {
+        System.out.println("5 + 3 is " + 8);
+    }
+}
