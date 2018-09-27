2ef5d40b9909344f0e5d9f94f5445b7cb0cfc035
KenDinh <d7n1b@ugrad.cs.ubc.ca>
2018-09-27 16:05:18 -0700

    Finished P2

--- src/model/ToDoList.java
+++ src/model/ToDoList.java
@@ -1,16 +1,18 @@
-package ui;
+package model;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ToDoListApp {
+public class ToDoList {
 
     private static ArrayList<String> toDo = new ArrayList<>();
     private static ArrayList<String> crossedOff = new ArrayList<>();
 
-
-    public static void main(String[] args) {
+    //MODIFIES: this
+    //EFFECTS:  prints out a list of options for the user to choose from
+    //          execute either add, remove the last item, remove a specific item, show all items or quite based on user input
+    public ToDoList() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to ToDoListApp");
 
         while (true) {
             // Prints menu choices
@@ -20,32 +22,31 @@ public class ToDoListApp {
             System.out.println("[3] Remove a specific item from your ToDo list");
             System.out.println("[4] Show all items in Todo and Crossed Off lists.");
             System.out.println("[5] Quit application.");
-            int userInput = scanner.nextInt();
+            String userInput = scanner.next();
             scanner.nextLine();
 
-            if (userInput == 1) {
+            if (userInput.equals("1")) {
                 System.out.println("Enter your todo task: ");
                 String task = scanner.nextLine();
                 toDo.add(task);
                 System.out.println("");
-
-            } else if (userInput == 2) {
+            }
+            else if (userInput.equals("2")) {
                 if (toDo.size() == 0) {
                     System.out.println("The ToDo list is empty");
                     System.out.println("");
-                }
-                else{
-                String removed = toDo.get(toDo.size() -1);
-                    toDo.remove(toDo.size() -1);
+                } else {
+                    String removed = toDo.get(toDo.size() - 1);
+                    toDo.remove(toDo.size() - 1);
                     crossedOff.add(removed);
                     System.out.println("The last item in the list: " + "(" + removed + ")" + " is removed");
                     System.out.println("");
                 }
             }
-                else if (userInput == 3) {
+            else if (userInput.equals("3")) {
                 System.out.println("The items to be done are: ");
-                for (int i = 0; i < toDo.size(); i ++){
-                    System.out.println((i+1) + ". " + toDo.get(i) + "\n" );
+                for (int i = 0; i < toDo.size(); i++) {
+                    System.out.println((i + 1) + ". " + toDo.get(i) + "\n");
                 }
                 System.out.println("Enter the index of the item you want to cross off: ");
                 int index = scanner.nextInt();
@@ -56,28 +57,27 @@ public class ToDoListApp {
                     System.out.println("There is no item at the index " + index + " to be crossed off.");
                 }
             }
-
-            else if (userInput == 4) {
+            else if (userInput.equals("4")) {
                 System.out.println("The items that are still need to do are: " + "\n");
-                for (int i = 0; i < toDo.size(); i ++){
-                    System.out.println((i+1) + ". " + toDo.get(i));
+                for (int i = 0; i < toDo.size(); i++) {
+                    System.out.println((i + 1) + ". " + toDo.get(i));
                 }
                 System.out.println("");
                 System.out.println("The items that have been crossed off are: " + "\n");
-                for (int i = 0; i < crossedOff.size(); i ++){
-                    System.out.println((i+1) + ". " + crossedOff.get(i));
+                for (int i = 0; i < crossedOff.size(); i++) {
+                    System.out.println((i + 1) + ". " + crossedOff.get(i));
                     System.out.println("");
                 }
-
-
-            } else if (userInput == 5) {
+            }
+            else if (userInput.equals("5")) {
                 System.out.println("Thank you for using our application.");
                 break;
-            } else {
-                System.out.println("Sorry but you have chosen an invalid option. Please choose again.");
-
+            }
+            else {
+                System.out.println("Sorry but you have chosen an invalid option. Please choose again." +"\n");
 
             }
         }
     }
+
 }
---
+++ src/ui/Main.java
@@ -0,0 +1,10 @@
+package ui;
+import model.ToDoList;
+
+public class Main {
+
+    //Initiate the ToDoList application
+    public static void main(String[] args) {
+        ToDoList t = new ToDoList();
+    }
+}
